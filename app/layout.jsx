2969f4a0c99b84ea2c7d741cc966a56f43be836b
Rohith Sagar <rohithsagar14325@gmail.com>
2025-11-24 16:22:19 +0400

    sync to 24.11 repo

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import StoreProvider from "@/app/StoreProvider";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import React from 'react'
+import MetaPixel from "@/components/MetaPixel";
 
 const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
@@ -27,23 +28,7 @@ export default function RootLayout({ children }) {
             <link rel="preconnect" href={ikOrigin} crossOrigin="anonymous" />
           </>
         )}
-        {/* Meta Pixel Code */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1509420293656480');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
+          {/* Meta Pixel Code moved to Client Component */}
         <noscript>
           <img
             height="1"
@@ -53,8 +38,10 @@ export default function RootLayout({ children }) {
           />
         </noscript>
         {/* End Meta Pixel Code */}
+
       </head>
       <body className={`${outfit.className} antialiased`} suppressHydrationWarning>
+          <MetaPixel />
         {isClerkConfigured ? (
           <ClerkProvider publishableKey={clerkPk}>
             <StoreProvider>
